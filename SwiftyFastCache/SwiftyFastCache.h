172ddaaeb43168f2657e636e4fd12926097f4088
Javid Multani <javid.multani@contentcreation.com.my>
2019-05-16 15:33:34 +0800

    Initial project setup

---
+++ SwiftyFastCache/SwiftyFastCache.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyFastCache.h
+//  SwiftyFastCache
+//
+//  Created by CCC on 16/05/2019.
+//  Copyright © 2019 Javedmultani16. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyFastCache.
+FOUNDATION_EXPORT double SwiftyFastCacheVersionNumber;
+
+//! Project version string for SwiftyFastCache.
+FOUNDATION_EXPORT const unsigned char SwiftyFastCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyFastCache/PublicHeader.h>
+
+
---
+++ SwiftyFastCacheExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyFastCacheExample
+//
+//  Created by CCC on 16/05/2019.
+//  Copyright © 2019 Javedmultani16. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
